2b0a3e4a78522a128cbcad300622dde133c017cb
D. Shwagginz <93965353+D-Shwagginz@users.noreply.github.com>
2023-08-26 20:35:07 -0700

    Doom sound to wav (#6)
    * Finished sound to wad
    
    * Final
    
    ---------
    
    Co-authored-by: D. Shwagginz <dshwagbusiness@gmail.com>

--- spec/wad-reader_spec.cr
+++ spec/wad-reader_spec.cr
@@ -121,6 +121,41 @@ describe WAD do
     mywad.sounds.find! { |m| m.name == "DSSAWIDL" }.sample_rate.should eq 11025
     mywad.sounds.find! { |m| m.name == "DSRLAUNC" }.samples_num.should eq 15483
     mywad.sounds.find! { |m| m.name == "DSSWTCHX" }.samples[0].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[0].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[1].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[2].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[3].should eq 127
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[4].should eq 128
     mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[11].should eq 129
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[-1].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[-2].should eq 129
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[-3].should eq 128
+    mywad.sounds.find! { |m| m.name == "DSSLOP" }.samples[-4].should eq 128
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[0].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[1].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[2].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[3].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[-1].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[-2].should eq 122
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[-3].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPLPAIN" }.samples[-4].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[0].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[1].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[2].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[3].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[-1].should eq 126
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[-2].should eq 125
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[-3].should eq 124
+    mywad.sounds.find! { |m| m.name == "DSPDIEHI" }.samples[-4].should eq 124
+  end
+
+  it "should properly create sound file" do
+    mywad = WAD.read("./rsrc/DOOM.WAD")
+
+    File.open("./rsrc/spectest.wav", "w+") do |io|
+      mywad.sounds.find! { |m| m.name == "DSPISTOL" }.to_wav(io)
+    end
+    File.exists?("./rsrc/spectest.wav").should be_true
+    File.delete("./rsrc/spectest.wav")
   end
 end
